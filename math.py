47051e0f3036e431f94807cf9257839f10e01f6c
Suchitra Kadolkar <62543141+SuchitraKadolkar@users.noreply.github.com>
2021-07-02 14:20:44 +0530

    Initial commit of math.py

---
+++ math.py
@@ -0,0 +1,18 @@
+#Add implementation
+def add(x,y):
+	return x+y
+#Subtract implementation
+def sub(x,y):
+	return x-y   # on Bug456
+#Multiply implementation
+def mul(x,y):
+	return x*y  # on master
+#Divide implementation
+def div(x,y):
+	if y==0:
+		return "Divide by Zero error"
+	else:
+		return x/y
+		# on Bug789 
+def square(x):
+	pass
